76b2fcaafabfb2f3dab19220cf350520982caf85
Burny <gamingburny@gmail.com>
2018-01-13 16:26:46 +0100

    update streamtimers to 1.1.2

--- StreamTimers/ReadMe.txt
+++ StreamTimers/ReadMe.txt
@@ -5,12 +5,40 @@ Description: Uptime & Countdown with UI Interface
 Created by: 
 Brain - www.twitch.tv/wellbrained
 Burny - www.twitch.tv/burnysc2
-Version: 1.1.1
+Version: 1.1.2
+
+
+################
+Usage
+################
+This script creates two text files called "Uptime.txt" and "Countdown.txt" in the following path: "..\AnkhBotR2\Twitch\Files".
+For usage add them as textsource in your streaming software (OBS) or just by simply drag'n'drop into it.
+
+# Uptime.txt: 
+Displays the uptime of your running stream in the format you want.
+It will automatically start counting up when your stream is indicated as live on Twitch and will reset itself after your stream was offline longer then the "Max Sleep Timer".
+If your stream got interrupted by a crash on OBS or Twitch is having problems it stops counting up.
+If you reconnect/restart your stream within the "Max Sleep Timer" the uptime will continue again. 
+
+# Countdown.txt:
+Displays a simple countdown in a format you want and after the countdown reaches 0 it will show the text from the "Custom Text after Countdown" textbox.
+
+
+You can also set the countdown and uptime manually by using the commands in the respective area in the Scripts sidebar.
+"Set countdown chat command" & "Set uptime chat command".
+
+Use that specific command + the amount of time in minutes you want to set it to.
+Example: !countdown 5 (Sets the countdown to 5 minutes)
+Example: !setuptime 150 (Sets the uptime to 2 hours and 30 minutes)
 
 
 ###############
 Version History
 ###############
+1.1.2:
+ ~ If the stream was running and then turned offline, the script will run until "Max Sleep Timer" has been reached and then reset the uptime, if the stream has not gone back online in the meantime.
+ ~ If Streamlabs Chatbot is started while the stream is offline, it will also set the uptime back to zero.
+ ~ The commands are now used with minutes (previously seconds). "!countdown 5" will set it to 5 minutes.
 1.1.1:
  ~ Necessary changes since AnkhBot became Streamlabs Chatbot
 1.1.0:
--- StreamTimers/StreamTimers_StreamlabsSystem.py
+++ StreamTimers/StreamTimers_StreamlabsSystem.py
@@ -19,41 +19,41 @@ ScriptName = "StreamTimers"
 Website = "http://www.brains-world.eu"
 Description = "Uptime & Countdown with UI Interface"
 Creator = "Brain"
-Version = "1.1.1"
+Version = "1.1.2"
 
 configFile = "settings.json"
 path = os.path.dirname(__file__)
 timeFromLastTick = time.time()
 countdown = {
-	"oldCountdownText": "",
-	"countdownText": "",
-	"countdownFileName": "..\..\Files\Countdown.txt",
-	"currentCountdownTime": 0,
-	"countdownIsRunning": False,
+    "oldCountdownText": "",
+    "countdownText": "",
+    "countdownFileName": "..\..\Files\Countdown.txt",
+    "currentCountdownTime": 0,
+    "countdownIsRunning": False,
 }
 cdVariables = {
-	"$h$": 0,
-	"$m$": 0,
-	"$s$": 0,
-	"$hh$": "0",
-	"$mm$": "0",
-	"$ss$": "0",
+    "$h$": 0,
+    "$m$": 0,
+    "$s$": 0,
+    "$hh$": "0",
+    "$mm$": "0",
+    "$ss$": "0",
 }
 uptime = {
-	"oldUptimeText": "",
-	"uptimeText": "",
-	"uptimeFileName": "..\..\Files\Uptime.txt",
-	"uptimeJsonFileName": "Uptime.json",
-	"currentUptime": 0,
-	"twitchApiResponseOffline": 0,
+    "oldUptimeText": "",
+    "uptimeText": "",
+    "uptimeFileName": "..\..\Files\Uptime.txt",
+    "uptimeJsonFileName": "Uptime.json",
+    "currentUptime": 0,
+    "twitchApiResponseOffline": 0,
 }
 utVariables = {
-	"$h$": 0,
-	"$m$": 0,
-	"$s$": 0,
-	"$hh$": "0",
-	"$mm$": "0",
-	"$ss$": "0",
+    "$h$": 0,
+    "$m$": 0,
+    "$s$": 0,
+    "$hh$": "0",
+    "$mm$": "0",
+    "$ss$": "0",
 }
 
 settings = {}
@@ -64,227 +64,274 @@ threadsKeepAlive = True
 #---------------------------------------
 #   [Required] Initialize Data / Load Only
 #---------------------------------------
+
+
 def Init():
-	global configFile, path, settings, uptimeThreadActive, uptime, utVariables
-	path = os.path.dirname(__file__)
-	try:
-		with codecs.open(os.path.join(path, configFile), encoding='utf-8-sig', mode='r') as file:
-			settings = json.load(file, encoding='utf-8-sig')
-	except:
-		settings = {
-			"cdInterval": 6,
-			"cdTimeFormat": "$mm$:$ss$",
-			"cdCustomText": "Countdown done. Starting soon.",
-			"cbCustomUptimeText": True,
-			"customUptimeText": "$h$:$mm$:$ss$",
-			"maxSleepTimer": 10.0,
-		}
+    global configFile, path, settings, uptimeThreadActive, uptime, utVariables
+    path = os.path.dirname(__file__)
+    try:
+        with codecs.open(os.path.join(path, configFile), encoding='utf-8-sig', mode='r') as file:
+            settings = json.load(file, encoding='utf-8-sig')
+    except:
+        settings = {
+            "cdInterval": 6,
+            "cdTimeFormat": "$mm$:$ss$",
+            "cdCustomText": "Countdown done. Starting soon.",
+            "cbCustomUptimeText": True,
+            "customUptimeText": "$h$:$mm$:$ss$",
+            "maxSleepTimer": 10.0,
+        }
 
-	try:
-		path = os.path.dirname(__file__)
-		with codecs.open(os.path.join(path, uptime["uptimeJsonFileName"]), encoding='utf-8-sig', mode='r') as file:
-			uptime["currentUptime"] = json.load(file, encoding='utf-8-sig')["currentUptime"]
-	except:
-		pass
+    try:
+        path = os.path.dirname(__file__)
+        with codecs.open(os.path.join(path, uptime["uptimeJsonFileName"]), encoding='utf-8-sig', mode='r') as file:
+            uptime["currentUptime"] = json.load(
+                file, encoding='utf-8-sig')["currentUptime"]
+    except:
+        pass
 
-	return
+    if not uptimeThreadActive and not Parent.IsLive():
+        uptime["currentUptime"] = 0
+        SetUptimeVariables()
+        WriteUptimeToFile()
+    return
 
 
 #---------------------------------------
 #	[Required] Execute Data / Process Messages
 #---------------------------------------
 def Execute(data):
-	global settings, cdVariables, countdown, uptime, utVariables
+    global settings, cdVariables, countdown, uptime, utVariables
 
-	if data.IsChatMessage():
-		# sets new countdown and starts it
-		if data.GetParamCount() == 2 and data.GetParam(0).lower() == settings["cdSetCountdown"].lower():
-			if Parent.HasPermission(data.User, "Caster", ""):
-				try:
-					countdown["currentCountdownTime"] = int(data.GetParam(1))
-					if not countdown["countdownIsRunning"]:
-						StartCountdown()
-					if settings["cdShowCountdownResponse"]:
-						Parent.SendTwitchMessage(("New countdown set to " + AutoFormat(*ConvertSecondsToHMS(int(data.GetParam(1)))))[:490])
-				except ValueError:
-					if settings["cdShowCountdownResponse"]:
-						Parent.SendTwitchMessage(("Incorrect usage. Write " + settings["cdSetCountdown"] + " <seconds> to set new the countdown.")[:490])
+    if data.IsChatMessage():
+        # sets new countdown and starts it
+        if data.GetParamCount() == 2 and data.GetParam(0).lower() == settings["cdSetCountdown"].lower():
+            if Parent.HasPermission(data.User, "Caster", ""):
+                try:
+                    countdown["currentCountdownTime"] = int(data.GetParam(1)) * 60
+                    if not countdown["countdownIsRunning"]:
+                        StartCountdown()
+                    if settings["cdShowCountdownResponse"]:
+                        Parent.SendTwitchMessage(
+                            ("New countdown set to " + AutoFormat(*ConvertSecondsToHMS(int(data.GetParam(1)) * 60)))[:490])
+                except ValueError:
+                    if settings["cdShowCountdownResponse"]:
+                        Parent.SendTwitchMessage(
+                            ("Incorrect usage. Write " + settings["cdSetCountdown"] + " <seconds> to set new the countdown.")[:490])
 
-		# sets a custom uptime
-		if data.GetParamCount() == 2 and data.GetParam(0).lower() == settings["setUptime"].lower():
-			if Parent.HasPermission(data.User, "Caster", ""):
-				try:
-					uptime["currentUptime"] = int(data.GetParam(1))
-					if settings["showUptimeResponse"]:
-						Parent.SendTwitchMessage(("Uptime set to " + AutoFormat(*ConvertSecondsToHMS(int(data.GetParam(1)))))[:490])
-				except ValueError:
-					if settings["showUptimeResponse"]:
-						Parent.SendTwitchMessage(("Incorrect usage. Write " + settings["setUptime"] + " <seconds> to set the uptime.")[:490])
+        # sets a custom uptime
+        if data.GetParamCount() == 2 and data.GetParam(0).lower() == settings["setUptime"].lower():
+            if Parent.HasPermission(data.User, "Caster", ""):
+                try:
+                    uptime["currentUptime"] = int(data.GetParam(1)) * 60
+                    SetUptimeVariables()
+                    WriteUptimeToFile()
+                    if settings["showUptimeResponse"]:
+                        Parent.SendTwitchMessage(
+                            ("Uptime set to " + AutoFormat(*ConvertSecondsToHMS(int(data.GetParam(1)) * 60)))[:490])
+                except ValueError:
+                    if settings["showUptimeResponse"]:
+                        Parent.SendTwitchMessage(
+                            ("Incorrect usage. Write " + settings["setUptime"] + " <seconds> to set the uptime.")[:490])
 
 
 #---------------------------------------
 # Reload Settings on Save
 #---------------------------------------
 def ReloadSettings(jsonData):
-	Init()
-	return
+    Init()
+    return
+
 
 def StartCountdown():
-	global countdown, settings, countdownThreadActive
-	countdown["countdownIsRunning"] = True
-	if not countdownThreadActive:
-		threading.Thread(target=CountdownThread, args=()).start()
+    global countdown, settings, countdownThreadActive
+    countdown["countdownIsRunning"] = True
+    if not countdownThreadActive:
+        threading.Thread(target=CountdownThread, args=()).start()
+
 
 def ResetCountdown():
-	global countdown, settings
-	countdown["currentCountdownTime"] = settings["cdInterval"] * 60
-	countdown["countdownIsRunning"] = False
-	countdown["oldCountdownText"] = " "
-	with codecs.open(os.path.join(path, countdown["countdownFileName"]), encoding='utf-8-sig', mode='w+') as file:
-		file.write(" ")
+    global countdown, settings
+    countdown["currentCountdownTime"] = settings["cdInterval"] * 60
+    countdown["countdownIsRunning"] = False
+    countdown["oldCountdownText"] = " "
+    with codecs.open(os.path.join(path, countdown["countdownFileName"]), encoding='utf-8-sig', mode='w+') as file:
+        file.write(" ")
+
 
 def CountdownThread():
-	global cdVariables, countdown, settings, timeFromLastTick, countdownThreadActive, threadsKeepAlive
-	countdownThreadActive = True
+    global cdVariables, countdown, settings, timeFromLastTick, countdownThreadActive, threadsKeepAlive
+    countdownThreadActive = True
 
-	while countdown["countdownIsRunning"] and threadsKeepAlive:
-		countdown["currentCountdownTime"] -= 1
-		tempMinutes, tempSeconds = divmod(countdown["currentCountdownTime"], 60)
-		tempHours, tempMinutes = divmod(tempMinutes, 60)
-		cdVariables["$h$"] = str(int(tempHours))
-		cdVariables["$m$"] = str(int(tempMinutes))
-		cdVariables["$s$"] = str(int(tempSeconds))
-		cdVariables["$hh$"] = str(int(tempHours)).zfill(2)
-		cdVariables["$mm$"] = str(int(tempMinutes)).zfill(2)
-		cdVariables["$ss$"] = str(int(tempSeconds)).zfill(2)
+    while countdown["countdownIsRunning"] and threadsKeepAlive:
+        countdown["currentCountdownTime"] -= 1
+        tempMinutes, tempSeconds = divmod(
+            countdown["currentCountdownTime"], 60)
+        tempHours, tempMinutes = divmod(tempMinutes, 60)
+        cdVariables["$h$"] = str(int(tempHours))
+        cdVariables["$m$"] = str(int(tempMinutes))
+        cdVariables["$s$"] = str(int(tempSeconds))
+        cdVariables["$hh$"] = str(int(tempHours)).zfill(2)
+        cdVariables["$mm$"] = str(int(tempMinutes)).zfill(2)
+        cdVariables["$ss$"] = str(int(tempSeconds)).zfill(2)
 
-		cdVariables["$autoFormat$"] = AutoFormat(cdVariables["$h$"], cdVariables["$m$"], cdVariables["$s$"])
+        cdVariables["$autoFormat$"] = AutoFormat(
+            cdVariables["$h$"], cdVariables["$m$"], cdVariables["$s$"])
 
-		countdown["countdownText"] = FormatCountdownString(settings["cdTimeFormat"])
+        countdown["countdownText"] = FormatCountdownString(
+            settings["cdTimeFormat"])
 
-		#Debug(settings["cdTimeFormat"] + " " + countdown["countdownText"])
+        #Debug(settings["cdTimeFormat"] + " " + countdown["countdownText"])
 
-		if countdown["currentCountdownTime"] < 0:
-			countdown["countdownText"] = settings["cdCustomText"]
-			countdown["countdownIsRunning"] = False
+        if countdown["currentCountdownTime"] < 0:
+            countdown["countdownText"] = settings["cdCustomText"]
+            countdown["countdownIsRunning"] = False
 
-		if countdown["oldCountdownText"] != countdown["countdownText"]:
-			with codecs.open(os.path.join(path, countdown["countdownFileName"]), encoding='utf-8-sig', mode="w+") as file:
-				file.write(countdown["countdownText"])
-			countdown["oldCountdownText"] = countdown["countdownText"]
-		time.sleep(1)
-	countdownThreadActive = False
+        if countdown["oldCountdownText"] != countdown["countdownText"]:
+            with codecs.open(os.path.join(path, countdown["countdownFileName"]), encoding='utf-8-sig', mode="w+") as file:
+                file.write(countdown["countdownText"])
+            countdown["oldCountdownText"] = countdown["countdownText"]
+        time.sleep(1)
+    countdownThreadActive = False
+
 
 def FormatCountdownString(string):
-	global cdVariables, countdown
-	for variable, text in cdVariables.items():
-		string = string.replace(variable, text)
-	return string
+    global cdVariables, countdown
+    for variable, text in cdVariables.items():
+        string = string.replace(variable, text)
+    return string
+
 
 def ResetUptime():
-	global uptime, settings
-	uptime["currentUptime"] = 0
-	uptime["twitchApiResponseOffline"] = 0
-	with codecs.open(os.path.join(path, uptime["uptimeFileName"]), encoding='utf-8-sig', mode='w+') as file:
-		file.write(" ")
-	with codecs.open(os.path.join(path, uptime["uptimeJsonFileName"]), encoding='utf-8-sig', mode='w+') as file:
-		json.dump({"currentUptime": uptime["currentUptime"]}, file)
+    global uptime, settings
+    uptime["currentUptime"] = 0
+    uptime["twitchApiResponseOffline"] = 0
+    with codecs.open(os.path.join(path, uptime["uptimeFileName"]), encoding='utf-8-sig', mode='w+') as file:
+        file.write(" ")
+    with codecs.open(os.path.join(path, uptime["uptimeJsonFileName"]), encoding='utf-8-sig', mode='w+') as file:
+        json.dump({"currentUptime": uptime["currentUptime"]}, file)
+
 
 def UptimeThread():
-	global utVariables, uptime, uptimeThreadActive, threadsKeepAlive
+    global utVariables, uptime, uptimeThreadActive, threadsKeepAlive
 
-	uptimeThreadActive = True
-	while uptime["twitchApiResponseOffline"] < (settings["maxSleepTimer"] * 60) and threadsKeepAlive:
-		if Parent.IsLive():
-			uptime["twitchApiResponseOffline"] = 0
-		else:
-			uptime["twitchApiResponseOffline"] += 1
+    uptimeThreadActive = True
+    while uptime["twitchApiResponseOffline"] < (settings["maxSleepTimer"] * 60) and threadsKeepAlive:
+        if Parent.IsLive():
+            uptime["twitchApiResponseOffline"] = 0
+        else:
+            uptime["twitchApiResponseOffline"] += 1
 
-		uptime["currentUptime"] += 1
-		utVariables["$m$"], utVariables["$s$"] = divmod(uptime["currentUptime"], 60)
-		utVariables["$h$"], utVariables["$m$"] = divmod(utVariables["$m$"], 60)
+        # once the "Max Sleep Timer" has been reached, it will reset the uptime to 0:00:00
+        if uptime["twitchApiResponseOffline"] >= (settings["maxSleepTimer"] * 60):
+            uptime["currentUptime"] = 0
+        else:
+            uptime["currentUptime"] += 1
 
-		utVariables["$h$"] = int(utVariables["$h$"])
-		utVariables["$m$"] = int(utVariables["$m$"])
-		utVariables["$s$"] = int(utVariables["$s$"])
+        SetUptimeVariables()
+        WriteUptimeToFile()
+        time.sleep(1)
 
-		utVariables["$hh$"] = str(utVariables["$h$"]).zfill(2)
-		utVariables["$mm$"] = str(utVariables["$m$"]).zfill(2)
-		utVariables["$ss$"] = str(utVariables["$s$"]).zfill(2)
+    uptimeThreadActive = False
 
-		utVariables["$autoFormat$"] = AutoFormat(utVariables["$h$"], utVariables["$m$"], utVariables["$s$"])
 
-		uptime["uptimeText"] = FormatUptimeString(settings["customUptimeText"])
-		#Debug(settings["customUptimeText"] + " " + uptime["uptimeText"])
-		if uptime["uptimeText"] != uptime["oldUptimeText"]:
-			with codecs.open(os.path.join(path, uptime["uptimeJsonFileName"]), encoding='utf-8-sig', mode='w+') as file:
-				json.dump({"currentUptime": uptime["currentUptime"]}, file)
+def SetUptimeVariables():
+    global utVariables, settings, uptime
+    utVariables["$m$"], utVariables["$s$"] = divmod(
+        uptime["currentUptime"], 60)
+    utVariables["$h$"], utVariables["$m$"] = divmod(utVariables["$m$"], 60)
 
-			with codecs.open(os.path.join(path, uptime["uptimeFileName"]), encoding='utf-8-sig', mode='w+') as file:
-				file.write(uptime["uptimeText"])
+    utVariables["$h$"] = int(utVariables["$h$"])
+    utVariables["$m$"] = int(utVariables["$m$"])
+    utVariables["$s$"] = int(utVariables["$s$"])
 
-			uptime["oldUptimeText"] = uptime["uptimeText"]
-		time.sleep(1)
-	uptimeThreadActive = False
+    utVariables["$hh$"] = str(utVariables["$h$"]).zfill(2)
+    utVariables["$mm$"] = str(utVariables["$m$"]).zfill(2)
+    utVariables["$ss$"] = str(utVariables["$s$"]).zfill(2)
+
+    utVariables["$autoFormat$"] = AutoFormat(
+        utVariables["$h$"], utVariables["$m$"], utVariables["$s$"])
+
+    uptime["uptimeText"] = FormatUptimeString(settings["customUptimeText"])
+
+
+def WriteUptimeToFile():
+    global uptime
+    if uptime["uptimeText"] != uptime["oldUptimeText"]:
+        with codecs.open(os.path.join(path, uptime["uptimeJsonFileName"]), encoding='utf-8-sig', mode='w+') as file:
+            json.dump({"currentUptime": uptime["currentUptime"]}, file)
+
+        with codecs.open(os.path.join(path, uptime["uptimeFileName"]), encoding='utf-8-sig', mode='w+') as file:
+            file.write(uptime["uptimeText"])
+
+        uptime["oldUptimeText"] = uptime["uptimeText"]
+
 
 def FormatUptimeString(string):
-	global utVariables, uptime
-	for variable, text in utVariables.items():
-		string = string.replace(variable, str(text))
-	return string
+    global utVariables, uptime
+    for variable, text in utVariables.items():
+        string = string.replace(variable, str(text))
+    return string
+
 
 def ConvertSecondsToHMS(seconds):
-	secs = int(seconds)
-	mins, secs = divmod(secs, 60)
-	hours, mins = divmod(mins, 60)
-	return hours, mins, secs
+    secs = int(seconds)
+    mins, secs = divmod(secs, 60)
+    hours, mins = divmod(mins, 60)
+    return hours, mins, secs
+
 
 def AutoFormat(hours, minutes, seconds):
-	hours = int(hours)
-	minutes = int(minutes)
-	seconds = int(seconds)
-	returnString = ""
-	seperator = ":"
-	if hours > 0:
-		returnString += str(hours) + seperator
-		returnString += str(minutes).zfill(2) + seperator
-		returnString += str(seconds).zfill(2)
-	else:
-		if minutes > 0:
-			returnString += str(minutes) + seperator
-			returnString += str(seconds).zfill(2)
-		else:
-			returnString += str(seconds)
-	return returnString
+    hours = int(hours)
+    minutes = int(minutes)
+    seconds = int(seconds)
+    returnString = ""
+    seperator = ":"
+    if hours > 0:
+        returnString += str(hours) + seperator
+        returnString += str(minutes).zfill(2) + seperator
+        returnString += str(seconds).zfill(2)
+    else:
+        if minutes > 0:
+            returnString += str(minutes) + seperator
+            returnString += str(seconds).zfill(2)
+        else:
+            returnString += str(seconds)
+    return returnString
 
 #---------------------------------------
 #	[Required] Tick Function
 #---------------------------------------
+
+
 def Tick():
-	global uptimeThreadActive
-	if Parent.IsLive():
-		if not uptimeThreadActive:
-			threading.Thread(target=UptimeThread, args=()).start()
-	return
+    global uptimeThreadActive
+    if not uptimeThreadActive and Parent.IsLive():
+        uptimeThreadActive = True
+        threading.Thread(target=UptimeThread, args=()).start()
+    return
+
 
 def ScriptToggled(state):
-	global threadsKeepAlive
-	# if enabled again tell the script to keep the threads running again
-	if state:
-		threadsKeepAlive = True
-	# if the script gets disabled, stop all timers and resets the textfiles
-	else:
-		ResetCountdown()
-		ResetUptime()
-		uptime["twitchApiResponseOffline"] = 0
-		Unload()
-	return
+    global threadsKeepAlive
+    # if enabled again tell the script to keep the threads running again
+    if state:
+        threadsKeepAlive = True
+    # if the script gets disabled, stop all timers and resets the textfiles
+    else:
+        ResetCountdown()
+        ResetUptime()
+        uptime["twitchApiResponseOffline"] = 0
+        Unload()
+    return
+
 
 def Unload():
-	global threadsKeepAlive
-	threadsKeepAlive = False
-	return
+    global threadsKeepAlive
+    threadsKeepAlive = False
+    return
+
 
 def Debug(message):
-	if debuggingMode:
-		Parent.Log("StreamTimers", message)
+    if debuggingMode:
+        Parent.Log("StreamTimers", message)
